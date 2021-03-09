24bd6469b3f3fb6223b7515e8404b82ff255b523
Krzysiek Swol <64749798+krzysiekswol@users.noreply.github.com>
2021-03-09 18:18:22 +0100

    url find

--- js/app.js
+++ js/app.js
@@ -1,16 +1,18 @@
- let input = document.getElementById('search');
- 
- 
+let input = document.getElementById('search');
+console.log(input.value);
+
+
 searchPhotos = () => {
-   let key= `WSkFlomRD-nNTRdXWHDwj4pKPvJU2n3IBJLc6HQ4LiQ`;
-   let query = document.getElementById("search").value;
-   let url= `https://api.unsplash.com/search/photos/?client_id=${key}&query=${query}`
+let key= `WSkFlomRD-nNTRdXWHDwj4pKPvJU2n3IBJLc6HQ4LiQ`;
+let query = document.getElementById("search").value;
+let url= `https://api.unsplash.com/search/photos/?client_id=${key}&query=${query}&per_page=20`
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function (data) {
+    console.log(data);
+  
     
-   fetch(url)
-   .then((resp) => resp.json())
-   .then(function (data) {
-     console.log(data);
-   });
+  });
 }
 input.addEventListener('keypress', function(e) { 
   if(e.keyCode === 13) {
@@ -18,3 +20,22 @@ input.addEventListener('keypress', function(e) {
   searchPhotos();
   }
  })
+ 
+ input.addEventListener('keyup', function(e) {
+  if(input.value.length > 2) {
+let key= `WSkFlomRD-nNTRdXWHDwj4pKPvJU2n3IBJLc6HQ4LiQ`;
+let query = document.getElementById("search").value;
+let url= `https://api.unsplash.com/search/photos/?client_id=${key}&query=${query}=$per_page20`
+  fetch(url)
+  .then((resp) => resp.json())
+  .then(function (data) {
+    let arr1 = data.results.filter(item => item)
+    console.log(arr1[0].urls.thumb)
+
+    // let img = document.createElement('img').src= `${(arr1[0].urls.thumb)}`
+    // document.getElementsByClassName('shown').appendChild(img);
+    
+      
+    })
+    
+  }})
